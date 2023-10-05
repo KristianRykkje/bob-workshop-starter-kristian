2fbfd44f1ec3c82be8b13c97c552a6611c5b0b5a
KristianRykkje <kriryk@gmail.com>
2023-10-05 18:17:16 +0200

    lagt til access key

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -34,7 +34,7 @@ function ContactForm() {
         },
         body: JSON.stringify({
           ...formData,
-          access_key: "HER-MÅ-DU-LEGGE-INN-DIN-ACCESS-KEY", // Get your access key from https://web3forms.com/
+          access_key: "53fc192e-32f8-43ff-a7b3-ddbc1443939f", // Get your access key from https://web3forms.com/
         }),
       });
 
